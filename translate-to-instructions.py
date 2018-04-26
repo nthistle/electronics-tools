6967769fcfe83907004e3b89d154903f23dbb7eb
Neil Thistlethwaite <neilt1111@yahoo.com>
2018-04-25 22:23:22 -0400

    Finish basic translation functionality

--- translate-to-instructions.py
+++ translate-to-instructions.py
@@ -1,6 +1,35 @@
 import sys
 import json
 
+
+## Format of instructions:
+## (mnemonic, syntax, mode, opcode, bytes, mpu cycles)
+
+def translate_instructions(raw, ins_set):
+    instruction_dict = {}
+    for instruction in ins_set:
+        # Creating a mapping from opcode to instruction
+        instruction_dict[instruction[3][1:].upper()] = instruction
+    output_str = ""
+    bytes_left = 0
+
+    raw = raw.upper()
+    if " " in raw.strip() and max(len(t) for t in raw.split())==2:
+        raw = raw.split() # If space split
+    else:
+        raw = [raw[i:i+2] for i in range(0,len(raw),2)] # If conjoined
+        
+    for byte in raw:
+        if bytes_left > 0:
+            output_str += " " + byte
+            bytes_left -= 1
+        else:
+            output_str += "\n" + instruction_dict[byte][0]
+            bytes_left = instruction_dict[byte][4]-1
+
+    return output_str[1:]
+        
+
 def interactive_mode():
     default_file = "6800-instruction-set.json"
     instruction_set_filename = input("Instruction Set Filename? [%s] " % default_file)
@@ -12,11 +41,17 @@ def interactive_mode():
     except:
         print("Error reading file or parsing JSON!")
         sys.exit(1)
+    print()
     print("Instruction set loaded!")
     for attr in ["name","version","source"]:
         if attr in instruction_info:
             print("%s: %s" % (attr[0].upper()+attr[1:], instruction_info[attr]))
     instruction_set = instruction_info["instruction_set"]
+    print("%d instructions" % len(instruction_set))
+    print()
+    print("Input raw: ")
+    raw_program = input()
+    print(translate_instructions(raw_program, instruction_set))
 
 
 if __name__ == "__main__":
